d7004537c903f6e281d3542f369236c9eae6fe6c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-02 23:40:41 +0200

    feat: change database url

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
-  const connectionString = process.env.DATABASE_URL_V2;
+  const connectionString = process.env.DATABASE_URL;
   return mongoose.connect(connectionString);
 };
 
