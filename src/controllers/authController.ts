dd2023d82ae916de6f847b9c41d78e69da31112f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 01:20:26 +0200

    feat: send status code along with user data

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { IUserRequest } from "../types/interfaces";
 import { User } from "../models/User";
+import { StatusCodes } from "http-status-codes";
 export const registerController = async (req: IUserRequest, res: Response) => {
   const newUser = await User.create(req.body);
-  res.json(newUser);
+  res.status(StatusCodes.CREATED).json(newUser);
 };
 
 export const loginController = (_req: Request, res: Response) => {
