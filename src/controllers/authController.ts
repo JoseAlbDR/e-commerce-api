56ca1ab590057dddd1bcddace6ec13b64dff9518
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 01:24:54 +0200

    feat: check for first user registration to be admin

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,7 +3,13 @@ import { IUserRequest } from "../types/interfaces";
 import { User } from "../models/User";
 import { StatusCodes } from "http-status-codes";
 export const registerController = async (req: IUserRequest, res: Response) => {
-  const newUser = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const newUser = await User.create({ name, email, password, role });
   res.status(StatusCodes.CREATED).json(newUser);
 };
 
