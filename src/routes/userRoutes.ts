f65ffe531eb995a4e1ea48ec197db956a44cfb45
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 16:34:34 +0200

    feat: userRoutes with getAllUsers route

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  getAllUsers,
+  // showCurrentUser,
+  // getSingleUser,
+  // updateUser,
+  // updateUserPassword,
+} from "../controllers/userController";
+
+const router = express.Router();
+
+router.route("/").get(getAllUsers);
