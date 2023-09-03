567922994449dde0315489b6bf6c179877fe4744
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 16:05:21 +0200

    feat: validateBody(validateLogin) middleware in loginController

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,10 +4,12 @@ import {
   logoutController,
   registerController,
 } from "../controllers/authController";
+import { validateBody } from "../middleware/joi-validation";
+import { validateLogin } from "../utils/joiValidation";
 
 const router = express.Router();
 
-router.post("/login", loginController);
+router.post("/login", validateBody(validateLogin), loginController);
 router.post("/register", registerController);
 router.get("/logout", logoutController);
 
