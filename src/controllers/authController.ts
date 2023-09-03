fdb87c108354ed673f9df6a2a6228a7e334c85dc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 12:03:10 +0200

    feat: send JWT token via cookies

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -18,6 +18,13 @@ export const registerController = async (req: IUserRequest, res: Response) => {
   };
 
   const token = createJWT(tokenUser);
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
   res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 
