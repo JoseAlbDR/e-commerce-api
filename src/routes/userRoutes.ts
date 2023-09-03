f5ed415728b9e8b1f8450095bcb7593f5e5d9162
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 20:35:10 +0200

    feat: routes setup for showCurrentUser, updateUser and updateUserPassword controllers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,15 +2,19 @@ import express from "express";
 import {
   getAllUsers,
   getSingleUser,
-  // showCurrentUser,
-  // getSingleUser,
-  // updateUser,
-  // updateUserPassword,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
 } from "../controllers/userController";
 
 const router = express.Router();
 
 router.route("/").get(getAllUsers);
+
+router.route("/showMe").get(showCurrentUser);
+router.route("/updateUser").post(updateUser);
+router.route("/updateUserPassword").post(updateUserPassword);
+
 router.route("/:id").get(getSingleUser);
 
 export default router;
