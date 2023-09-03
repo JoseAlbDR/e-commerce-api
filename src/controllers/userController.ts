deb04c97de78978a597ab99d3f404657367b8701
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-03 23:36:51 +0200

    refactor: use .select(-password) to get users/user without password property from db

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,41 +2,43 @@ import { Request, Response } from "express";
 import { User } from "../models/User";
 import { StatusCodes } from "http-status-codes";
 import { ISingleUserRequest } from "../types/userInterfaces";
-import { NotFoundError } from "../errors";
+import { NotFoundError, UnauthenticatedError } from "../errors";
 
-export const getAllUsers = async (_req: Request, res: Response) => {
-  const users = await User.find({ role: "user" });
+export const getAllUsers = async (req: Request, res: Response) => {
+  const { role } = req.user;
 
-  const resultUsers = users.map(({ name, _id, email, role }) => {
-    return { userId: _id, name, email, role };
-  });
+  if (role !== "admin") {
+    throw new UnauthenticatedError("Only admin can get all users");
+  }
 
-  res.status(StatusCodes.OK).json({ users: resultUsers });
+  const users = await User.find({ role: "user" }).select("-password");
+
+  // const resultUsers = users.map(({ name, _id, email, role }) => ({
+  //   userId: _id,
+  //   name,
+  //   email,
+  //   role,
+  // }));
+
+  res.status(StatusCodes.OK).json({ users });
 };
 export const getSingleUser = async (req: ISingleUserRequest, res: Response) => {
   const { id } = req.params;
 
-  const user = await User.findById(id);
+  const user = await User.findById(id).select("-password");
 
   if (!user) {
     throw new NotFoundError("User not found");
   }
 
-  res.status(StatusCodes.OK).json({
-    user: {
-      userId: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role,
-    },
-  });
+  res.status(StatusCodes.OK).json({ user });
 };
 export const showCurrentUser = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
-export const updateUser = async (_req: Request, res: Response) => {
-  res.send("Update User");
+export const updateUser = async (req: Request, res: Response) => {
+  res.json(req.body);
 };
-export const updateUserPassword = async (_req: Request, res: Response) => {
-  res.send("Update User Password");
+export const updateUserPassword = async (req: Request, res: Response) => {
+  res.json(req.body);
 };
