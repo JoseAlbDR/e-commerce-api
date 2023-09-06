f3860829ce3143b5525c726690ac753584741f19
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-06 14:28:50 +0200

    feat: updateUser controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,25 +4,21 @@ import { StatusCodes } from "http-status-codes";
 import {
   ISingleUserRequest,
   IUpdatePasswordRequest,
+  IUpdateUserRequest,
 } from "../types/userInterfaces";
 import {
   BadRequestError,
   NotFoundError,
   UnauthenticatedError,
 } from "../errors";
+import { createTokenUser } from "../utils/createTokenUser";
+import { attachCookiesToResponse } from "../utils";
 
 export const getAllUsers = async (_req: Request, res: Response) => {
   const users = await User.find({ role: "user" }).select("-password");
-
-  // const resultUsers = users.map(({ name, _id, email, role }) => ({
-  //   userId: _id,
-  //   name,
-  //   email,
-  //   role,
-  // }));
-
   res.status(StatusCodes.OK).json({ users });
 };
+
 export const getSingleUser = async (req: ISingleUserRequest, res: Response) => {
   const { id } = req.params;
   console.log(req.user);
@@ -34,11 +30,23 @@ export const getSingleUser = async (req: ISingleUserRequest, res: Response) => {
 
   res.status(StatusCodes.OK).json({ user });
 };
+
 export const showCurrentUser = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
-export const updateUser = async (req: Request, res: Response) => {
-  res.json(req.body);
+
+export const updateUser = async (req: IUpdateUserRequest, res: Response) => {
+  const updatedUser = await User.findOneAndUpdate(
+    { _id: req.user.userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (updatedUser) {
+    const tokenUser = createTokenUser(updatedUser);
+    attachCookiesToResponse({ res, user: tokenUser });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+  }
 };
 
 export const updateUserPassword = async (
