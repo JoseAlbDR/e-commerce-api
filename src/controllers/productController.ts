e428af1f524595741dd7199acef9e083e5776ada
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-07 00:06:39 +0200

    feat: updateProduct controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { IProduct, IProductRequest } from "../types/productsInterfaces";
 import { Product } from "../models/Product";
 import { StatusCodes } from "http-status-codes";
+import { NotFoundError } from "../errors";
 export const createProduct = async (req: IProductRequest, res: Response) => {
   const product: IProduct = { ...req.body, user: req.user.userId };
   const newProduct = await Product.create(product);
@@ -17,12 +18,24 @@ export const getSingleProduct = async (req: Request, res: Response) => {
   const product = await Product.findById(req.params.id);
   res.status(StatusCodes.OK).json({ product });
 };
-export const updateProduct = async (_req: Request, res: Response) => {
-  res.send("update product");
+
+export const updateProduct = async (req: IProductRequest, res: Response) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  if (!product)
+    throw new NotFoundError(`Product with id ${req.params.id} not found`);
+  res.status(StatusCodes.OK).json({ product });
 };
-export const deleteProduct = async (_req: Request, res: Response) => {
-  res.send("delete product");
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  if (!product)
+    throw new NotFoundError(`Product with id ${req.params.id} not found`);
+  res.sendStatus(StatusCodes.OK);
 };
+
 export const uploadImage = async (_req: Request, res: Response) => {
   res.send("upload image");
 };
