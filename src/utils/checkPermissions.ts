dd9a90591984a97ec5f0cfe833c7ed1f053eeed1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-06 15:07:02 +0200

    feat: new util functio checkPermissions

---
+++ src/utils/checkPermissions.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { ITokenUser } from "../types/authInterfaces";
+import { UnauthorizedError } from "../errors";
+
+export const checkPermissions = (
+  requestUser: ITokenUser,
+  resourceUserId: mongoose.SchemaDefinitionProperty<mongoose.Types.ObjectId>
+) => {
+  console.log(requestUser);
+  console.log(resourceUserId);
+  console.log(typeof resourceUserId);
+
+  if (requestUser.role === "admin") return;
+  if (resourceUserId && typeof resourceUserId === "object") {
+    const userId = resourceUserId as mongoose.Types.ObjectId;
+    if (requestUser.userId === userId.toString()) return;
+  }
+
+  throw new UnauthorizedError("Not authorized to access this route");
+};
