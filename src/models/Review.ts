44df4ddcf30e1329c36646d6e1644f54175e5691
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-07 08:54:38 +0200

    feat: Review model

---
+++ src/models/Review.ts
@@ -0,0 +1,38 @@
+import { IReview } from "./../types/reviewInterfaces";
+import mongoose from "mongoose";
+
+const ReviewSchema = new mongoose.Schema<IReview>(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Plase provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide title"],
+      maxlength: [100, "Title cannot exceed 100 characters"],
+    },
+    comment: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide review text"],
+      maxlength: [1000, "Title cannot exceed 1000 characters"],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Review = mongoose.model("Review", ReviewSchema);
