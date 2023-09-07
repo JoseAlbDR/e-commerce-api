6e74094adf59a670243381f8250181968444b082
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-07 09:17:32 +0200

    feat: review routes with authentication and authorization middlewares

---
+++ src/routes/reviewRoutes.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  createReview,
+  deleteReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+} from "../controllers/reviewController";
+import {
+  authenticateUser,
+  authorizePermissions,
+} from "../middleware/authentication";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(getAllReviews)
+  .post(authenticateUser, authorizePermissions("user"), createReview);
+router
+  .route("/id")
+  .get(getSingleReview)
+  .patch(authenticateUser, authorizePermissions("user"), updateReview)
+  .delete(authenticateUser, authorizePermissions("user"), deleteReview);
