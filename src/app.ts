ddbdf9b667ef2571e1bd1bc8dc3dfb858252eebf
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-08 19:45:22 +0200

    feat: extra security packages

--- src/app.ts
+++ src/app.ts
@@ -14,31 +14,41 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
+// Security
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import xss from "./middleware/xssMiddleware";
+import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss);
+app.use(mongoSanitize());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./src/public"));
 app.use(fileUpload());
 
-app.get("/", (_req, res) => {
-  res.send("E-Commerce API");
-});
-
-app.get("/api/v1", (req, res) => {
-  // console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("E-Commerce API");
-});
-
+// Routers
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/reviews", reviewsRouter);
 app.use("/api/v1/orders", orderRouter);
 
+// Middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
